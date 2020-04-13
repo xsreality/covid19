0703f44d38645c35d8c707705d9d0e67446098cf
Abhinav Sonkar <xsabhinav@gmail.com>
2020-04-13 18:05:26 +0200

    Send summary updates of all Indian states every 4 hours to all subscribers. (#5)
    Signed-off-by: Abhinav Sonkar <xsabhinav@gmail.com>

--- covid19-telegram-bot/src/main/java/org/covid19/StatsAlertConsumerConfig.java
+++ covid19-telegram-bot/src/main/java/org/covid19/StatsAlertConsumerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.springframework.boot.ApplicationRunner;
@@ -19,6 +20,8 @@ import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,19 +30,28 @@ import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static org.covid19.TelegramUtils.buildStatewiseAlertText;
 import static org.covid19.TelegramUtils.fireStatewiseTelegramAlert;
 import static org.covid19.Utils.friendlyTime;
+import static org.covid19.Utils.initStateCodes;
 
 @EnableKafka
 @Configuration
+@EnableScheduling
+@Slf4j
 public class StatsAlertConsumerConfig {
-
     private final KafkaProperties kafkaProperties;
     private final Serde<String> stringSerde;
     private final Covid19Bot covid19Bot;
     private ReadOnlyKeyValueStore<String, StatewiseDelta> dailyStatsStore;
     private KafkaListenerEndpointRegistry registry;
+    final static Map<String, String> stateCodes;
+
+    static {
+        stateCodes = initStateCodes();
+    }
 
     public StatsAlertConsumerConfig(KafkaProperties kafkaProperties, Covid19Bot covid19Bot, KafkaListenerEndpointRegistry registry) {
         this.kafkaProperties = kafkaProperties;
@@ -47,6 +59,7 @@ public class StatsAlertConsumerConfig {
         this.registry = registry;
         this.stringSerde = Serdes.String();
     }
+
 
     @Bean
     public Map<String, Object> consumerConfigs() {
@@ -122,5 +135,39 @@ public class StatsAlertConsumerConfig {
         }
         fireStatewiseTelegramAlert(covid19Bot, buildStatewiseAlertText(friendlyTime(lastUpdated), readyToSend, dailyIncrements));
         readyToSend.clear();
+    }
+
+    @Scheduled(cron = "0 20 4,8,12,16,18 * * ?")
+    public void sendSummaryUpdates() {
+        final KeyValueIterator<String, StatewiseDelta> stats = dailyStatsStore.all();
+
+        List<StatewiseDelta> sortedStats = new ArrayList<>();
+        StatewiseDelta total = new StatewiseDelta();
+        stats.forEachRemaining(stat -> sortedStats.add(stat.value));
+        sortedStats.sort((o1, o2) -> (int) (o2.getCurrentConfirmed() - o1.getCurrentConfirmed()));
+
+        String lastUpdated = sortedStats.get(0).getLastUpdatedTime();
+
+        String text = String.format("<i>%s</i>\n\n", friendlyTime(lastUpdated));
+        text = text.concat("Summary of all affected Indian States\n\n");
+        text = text.concat("<pre>\n");
+        text = text.concat("State|  Conf|  Rec.| Died\n");
+        text = text.concat("-------------------------\n");
+        for (StatewiseDelta stat : sortedStats) {
+            if ("Total".equalsIgnoreCase(stat.getState())) {
+                total = stat;
+                continue; // show total at the end
+            }
+            if (stat.getCurrentConfirmed() < 1L && stat.getCurrentRecovered() < 1L && stat.getCurrentDeaths() < 1L) {
+                continue; // skip states with zero stats
+            }
+            text = text.concat(String.format("%-5s|%6s|%6s|%5s\n", stateCodes.get(stat.getState()), stat.getCurrentConfirmed(), stat.getCurrentRecovered(), stat.getCurrentDeaths()));
+        }
+        text = text.concat("-------------------------\n");
+        text = text.concat(String.format("%-5s|%6s|%6s|%5s\n", stateCodes.get(total.getState()), total.getCurrentConfirmed(), total.getCurrentRecovered(), total.getCurrentDeaths()));
+        text = text.concat("</pre>");
+
+        LOG.info("summary text {}", text);
+        fireStatewiseTelegramAlert(covid19Bot, text);
     }
 }
